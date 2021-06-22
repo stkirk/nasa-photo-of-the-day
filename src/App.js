fe7d393d734c5236cc65a00a43e76749983eb80d
stkirk <shaanekirk@gmail.com>
2021-06-22 15:41:21 -0400

    completed nav component with links and logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
+import { BASE_URL, API_KEY } from './constants/index';
+import Nav from './components/Nav'
+
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Nav/>
+      
     </div>
   );
 }

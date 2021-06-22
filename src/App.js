4a1ab0a32e66308bd5886563d7eff4718c8a0619
stkirk <shaanekirk@gmail.com>
2021-06-22 17:38:17 -0400

    succesfully requested data from nasa api, set that data as state and passed the data as props to PhotoCard component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios';
-import { API_KEY, BASE_URL } from '../constants/index';
+import { API_KEY, BASE_URL } from './constants/index';
 import Nav from './components/Nav'
 import PhotoCard from './components/PhotoCard'
 
 
 function App() {
   const [ data, setData] = useState();
+
+  useEffect(() => {
+    console.log('REQUEST URL:', `${BASE_URL}?api_key=${API_KEY}`);
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then(res => {
+      console.log('RES', res.data)
+      setData(res.data)
+    })
+    .catch(err => {debugger})
+  }, [])
+
   return (
     <div className="App">
       <Nav/>
-      {/* <PhotoCard/> */}
+      <PhotoCard data={data} />
     </div>
   );
 }
